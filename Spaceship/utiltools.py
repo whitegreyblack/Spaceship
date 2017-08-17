4b248e37198899d47de64ef40aac44de07c4e709
Sam Whang <sangwoowhang@gmail.com>
2017-08-17 13:59:27 -0500

    added lambdafunc for general-purpose tuple returns

--- Spaceship/utiltools.py
+++ Spaceship/utiltools.py
@@ -1,5 +1,9 @@
 # utiltools : functions for 2D graphics manipulations and transformation
 
+def lambdafunc(f, x1, y1, x2, y2):
+    '''Returns a tuple containing results using parameter function'''
+    return (f(x1, x2), f(y1, y2))
+
 def deltanorm(p1, p2):
     '''Returns direction of line given two points'''
     return int((p2-p1)/abs(p2-p1))
@@ -12,10 +16,18 @@ def basicmap(x, y, v=0):
     '''Returns 2D list with width x, height y, and values of z'''
     return [[0 for _ in range(y)] for _ in range(x)]
 
-def truecompare(p1, p2):
+def absdistance(p1, p2):
     '''Returns the distance between two points'''
     return abs(p1 - p2)
 
-def sortcompare(p1, p2):
+def absdistances(x1, y1, x2, y2):
+    '''Returns a tuple of distances for x, y'''
+    return (absdistance(x1, x2), absdistance(x1, x2))
+
+def maxmindistance(p1, p2):
     '''Returns the distance between two points'''
     return max(p1, p2) - min(p1, p2)
+
+def maxmindistances(x1, y1, x2, y2):
+    '''Returns a tuple of distances for x, y'''
+    return (maxmindistance(x1, x2), maxmindistance(y1, y2))
