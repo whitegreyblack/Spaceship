01f92b72a89c7d25cec69d2353acfc083b473c01
Sam Whang <sangwoowhang@gmail.com>
2017-08-17 16:56:21 -0500

    Fixed formatting

--- Spaceship/Textbox.py
+++ Spaceship/Textbox.py
@@ -4,6 +4,6 @@ class textbox:
     border = False
     color = (250, 250, 250)
     position = (0, 0)
-    def __init__(self, string, pos:
+    def __init__(self, string, pos):
         self.string = string
         self.pos = pos
