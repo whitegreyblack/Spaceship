1f321d78a01f61ca744815c34b20423895ae07de
Sam Whang <sangwoowhang@gmail.com>
2017-08-23 11:39:07 -0500

    Modified level test

--- spaceship/map_creation.py
+++ spaceship/map_creation.py
@@ -3,7 +3,8 @@
 from bearlibterminal import terminal as term
 from ctypes import c_uint32, addressof
 from namedlist import namedlist
-from time import time
+from time import time, sleep
+from copy import deepcopy
 
 limit_x, limit_y = 76, 20 # offset by -1 to account for start index 0
 factor_x, factor_y = 1, 1
@@ -18,38 +19,52 @@ movement_costs = {
 }
 
 wmap = """\
-####################
-#                  #
-#                  #
-#    #        #    #
-#                  #
-#                  #
-#                  #
-#                  #
-#    #        #    #
-#                  #
-#                  #
-####################
-"""
+#########################################################
+#.......................................................#
+#..................######################################
+#....#........#....#
+#..................#
+#..................#
+#..................#
+#..................#
+#....#........#....#
+#..................#
+#..................#
+####################"""
+
+copwmap = [[col for col in row] for row in wmap.split('\n')]
+for i in range(len(copwmap)):
+    print(len(copwmap[i]), copwmap[i])
+
 
 def movement(pos, change, factor, low, high):
     updated = pos + change * factor
     return updated if low < updated < high else max(low, min(updated, high))
 
 def test_map():
-    term.composition(True)
     global player_pos
     term.set("U+E003: ./assets/cursor03_small.png")
     term.clear()
     term.refresh()
     proceed = True
-    blockables = [(5,4), (5, 9), (14, 4), (14, 9)]
+    blockables =[]
+    units = []
+    #blockables = [(5,4), (5, 9), (14, 4), (14, 9)]
+    for i in range(len(copwmap)):
+        for j in range(len(copwmap[i])):
+            if copwmap[i][j] == "#":
+                blockables.append((j, i+1))
     try:
         while proceed:
             term.clear()
             term.puts(0, 0, 'Animating a map\n')
+            # for x,y in blockables:
+            #     term.puts(x, y, '#')
             x, y = player_pos
+
             term.puts(0, 1, wmap)
+            term.puts(5,10, '[color=orange]r[/color]')
+            units.append((5,10))
             term.puts(x, y, '@')
             while proceed and term.has_input():
                 term.puts(21, 5, 'Got input')
@@ -65,11 +80,29 @@ def test_map():
                     tx, ty = x, y
                     dx, dy = movement_costs[code]
                     fx, fy = movement_ratio
-                    x = movement(x, dx, fx, 1, 18)
-                    y = movement(y, dy, fy, 2, 11)
-                    if (x, y) in blockables:
-                        x, y = tx, ty
+                    if term.state(term.TK_SHIFT):
+                        # px, py = x, y
+                        term.puts(21, 6, 'Pressed Shift') 
+                        stop = False
+                        while not stop:
+                            x = movement(x, dx, fx, 1, 79) 
+                            y = movement(y, dy, fy, 2, 23)
+                            if (x, y) in blockables or (x, y) in units:
+                                x, y = tx, ty
+                                stop = True
+                            elif (tx, ty) == (x, y):
+                                stop = True
+                            tx, ty = x, y
+                    else:
+                        x = movement(x, dx, fx, 1, 79)
+                        y = movement(y, dy, fy, 2, 23)
+                        if (x, y) in blockables or (x, y) in units:
+                            x, y = tx, ty
                     player_pos = (x, y)
+                elif code in (term.TK_T,):
+                    term.puts(21, 7, 'talking')
+                elif code in (term.TK_S,):
+                    term.puts(21, 7, 'stealing')
                 else:
                     term.puts(0,6, 'other input')
             term.refresh()
