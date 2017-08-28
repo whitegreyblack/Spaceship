2a537cb6d70e5b318c5b0c5d2e7aa4752e8651b8
Sam Whang <sangwoowhang@gmail.com>
2017-08-28 13:31:29 -0500

    Modified main -- map works | need to work on blocking

--- spaceship/main.py
+++ spaceship/main.py
@@ -12,7 +12,7 @@ from random import randint
 # global terminal variables
 SCREEN_WIDTH, SCREEN_HEIGHT = 80, 24
 # LIMIT_FPS = 30 -- later used in sprite implementation
-
+blocked = []
 
 # ---------------------------------------------------------------------------------------------------------------------#
 # TERMINAL SETUP & IMAGE IMPORTS
@@ -67,11 +67,13 @@ def draw():
 # Start initializations
 setup()
 # global game variables
+FOV_RADIUS = 10
 MAP_WIDTH, MAP_HEIGHT = 24, 48
 MAP_FACTOR = 2
 COLOR_DARK_WALL = term.color_from_argb(128, 0, 0, 100)
 COLOR_DARK_GROUND = term.color_from_argb(128, 50, 50, 150)
-px, py = SCREEN_WIDTH//2, SCREEN_HEIGHT//2
+#px, py = SCREEN_WIDTH//2, SCREEN_HEIGHT//2
+px, py = 5, 5
 dungeon = Map(MAPS.DUNGEON)
 player = Object(px, py, '@')
 npc = Object(px-3, py-2, '@', 'orange')
@@ -79,9 +81,11 @@ units = [player, npc]
 proceed = True
 
 while proceed:
-    #dungeon.fov_calc(player.x, player.y)
     term.clear()
-    draw()
+    dungeon.fov_calc(player.x, player.y, FOV_RADIUS)
+    positions = [(x, y, lit, ch) for x, y, lit, ch in dungeon.output(player.x, player.y)]
+    for x, y, lit, ch in positions:
+        term.puts(x, y, "[color={}]{}[/color]".format(lit, ch))
     term.refresh()
     key_in()
 # End Initiailiation
