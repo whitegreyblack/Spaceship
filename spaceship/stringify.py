6aeca575c0d3b9cffc6d478074c46e2684db22d5
Sam Whang <sangwoowhang@gmail.com>
2017-08-29 10:53:07 -0500

    Modified stringify to use ascii characters by parameter

--- spaceship/stringify.py
+++ spaceship/stringify.py
@@ -1,29 +1,34 @@
 # using pil - transforms an image from png to a python string to be used as a map
 from PIL import Image
 
-def stringify(string, debug=False):
+characters = {
+    (0,0,0): ["#", "89"],
+    (185, 122, 87): ["+", "90"],
+    (112, 146, 190): ["~", "91"],
+    (255, 255, 255): [".", "92"],
+}
+
+def stringify(string, asciify=False, debug=False):
     with Image.open(string) as img:
         pixels = img.load()
         w, h = img.size
         lines = []
+        colors = set()
         for j in range(h):
             line = ""
             for i in range(w):
                 # sometimes alpha channel is included so test for all four values first
                 try:
-                    r, g, b, _ = pixels[i,j]
+                    r, g, b, _ = pixels[i, j]
                 except:
                     r, g, b = pixels[i, j]
-                if (r, g, b)== (0,0,0):
-                    line += "#"
-                # add some trees
-                # buildings
-                # some other stuff
-                else:
-                    line += "."
+                if (r,g,b) not in colors:
+                    colors.add((r,g,b))
+                line += characters[(r,g,b)][int(asciify)]
             lines.append(line)
         if debug:
             print("\n".join(lines))
+            print(colors)
         return "\n".join(lines)
 
 if __name__ == "__main__":
