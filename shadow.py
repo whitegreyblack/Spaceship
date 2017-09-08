01b9712fc036ed82d6846a911a3d9e5ed9f36178
Sam Whang <sangwoowhang@gmail.com>
2017-09-08 11:09:07 -0500

    Working on light class

---
+++ shadow.py
@@ -0,0 +1,10 @@
+"""File: Shadow.py 
+Holds the shadow class which implements vision around light/entity objects
+"""
+
+class Light: pass
+    def light(self, cx, cy, row, start, end, radius, xx, xy, yx, yy, id):
+        
+
+class Shadow: 
+    pass
