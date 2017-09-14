c38fd537f289130ecfedfb19e8d674bb350714f9
Sam Whang <sangwoowhang@gmail.com>
2017-09-14 11:58:36 -0500

    Starting on writing constants for proc gen in dungeons

--- spaceship/constants.py
+++ spaceship/constants.py
@@ -2,3 +2,14 @@
 SCREEN_WIDTH, SCREEN_HEIGHT = 80, 34
 FOV_RADIUS = 25
 
+
+# ITEM CONSTANTS
+ITEM_DROP_RATE=0 # drop rate from monsters in dungeons
+ITEM_FIND_RATE=0 # chances item spawns in dungeon
+ITEM_PREREVEAL=0 # basically pre identification rate 
+
+# ROOM CONSTANTS
+ROOM_MIN_PLACE=0
+ROOM_MAX_PLACE=0
+ROOM_HALL_SIZE=2
+ROOM_DOOR_RATE=0
