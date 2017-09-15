cec30d0297406ddfcadd6871f718425c1fa67091
Sam Whang <sangwoowhang@gmail.com>
2017-09-15 11:01:44 -0500

    Still working on generator -- todo: markov chain

--- spaceship/generator.py
+++ spaceship/generator.py
@@ -3,7 +3,14 @@ import sys
 import os
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__))+'/../')
 from collections import namedtuple
+from random import choice
 from enum import Enum
+
+objecttype = namedtuple("Object", "name inventory")
+
+MONSTER_LIST = ("RAT",)
+
+class ObjectTypeError(ValueError): pass
 
 class DataType(Enum):
     HUMAN = 0
@@ -11,16 +18,31 @@ class DataType(Enum):
 
 def build(data, container):
     """Abstract function which builds a new data object if implemented in container"""
-    if datatype.upper() not in keys(container):
-        raise ValueError("Data Type not yet implemented")
-    return datatype
+    if data.upper() not in keys(container):
+        raise ObjectTypeError("Data Type not yet implemented")
+    return objecttype(namebuilder(data), [])
 
-def keys(classname):
+def namebuilder(name):
+    """Checks name of data to see how to handle namebuilding"""
+    if name in MONSTER_LIST:
+        return name
+    else:
+        return randomFirstName(), randomLastName()
+
+def randomFirstName():
+    """Naive randomized name picker"""
+    return choice(["Eric", "Sam", "John"])
+
+def randomLastName():
+    """Naive randomized name picker"""
+    return choice(["Ericson", "Hunter", "Deere", "Livingston"])
+
+def keys(enumname):
     """Abstract function which returns keys from enum classes"""
-    return tuple(name for name, _ in classname.__members__.items())
+    return tuple(name for name, _ in enumname.__members__.items())
 
-print(build("rat"), DataType)
+print(build("rat", DataType))
 try:
-    print(build("do"), DataType)
+    print(build("do", DataType))
 except ValueError:
     print("pass")
