af11c6c6ddda3949abed3517b3d0ba043a1e2afb
Sam Whang <sangwoowhang@gmail.com>
2017-10-01 19:36:13 -0500

    Trying out new stats

---
+++ d2.py
@@ -0,0 +1,19 @@
+# Some formulas to use when developing a character
+from collections import namedtuple
+
+STATS = namedtuple("STATS", "str con int wis agi lck")
+MALE = STATS(1, 0, 0, 0, 0, 0)
+FEMALE = STATS(0, 0, 0, 0, 1, 0)
+
+HUMAN = STATS(3, 3, 3, 3, 3, 3)
+ELVEN = STATS(2, 2, 4, 3, 4, 3)
+ORCEN = STATS(4, 5, 2, 2, 3, 2)
+DWARF = STATS(3, 4, 2, 3, 3, 3)
+
+SQUIRE = STATS(1, 2, 0, 0, 1, 0)
+ARCHER = STATS(1, 1, 0, 0, 2, 1)
+
+
+print("RACE: ", sum(HUMAN), sum(ELVEN), sum(ORCEN), sum(DWARF))
+print("CLASS: ", sum(SQUIRE), sum(ARCHER))
+print("RACE + SEX: ", sum(x+y for x, y in zip(HUMAN, MALE)), sum(x+y for x, y in zip(HUMAN, FEMALE)))
