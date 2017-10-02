e9dc092bdedc99386502b518941430ae8922686e
Sam Whang <sangwoowhang@gmail.com>
2017-10-02 12:04:19 -0500

    refactored new name

--- spaceship/new_name.py
+++ spaceship/new_name.py
@@ -9,38 +9,44 @@ from spaceship.setup import setup, output, toChr
 from spaceship.maps import toInt
 
 def new_name(character) -> (int, str):
+    def text():
+        term.puts(center(direction_name, xhalf*2), yhalf-4, direction_name)
+        term.puts(center(direction_exit[2:], xhalf*2), yhalf+2, direction_exit)
+
     def border():
         for k in range(SCREEN_WIDTH):
             term.puts(k, 3, toChr("2550"))
             term.puts(k, SCREEN_HEIGHT-3, toChr("2550"))
-        for i in range(x-w, x+w):
-            term.puts(i, y-2, "{}".format(chr(toInt('2550'))))
-            term.puts(i, y, "{}".format(chr(toInt('2550'))))
-        for j in range(y-2, y+1):
-            term.puts(x-w, j, "{}".format(chr(toInt('2551'))))
-            term.puts(x+w, j, "{}".format(chr(toInt('2551'))))
-        term.puts(x-w, y-2, "{}".format(chr(toInt('2554'))))
-        term.puts(x+w, y-2, "{}".format(chr(toInt('2557'))))
-        term.puts(x-w, y, "{}".format(chr(toInt('255A'))))
-        term.puts(x+w, y, "{}".format(chr(toInt('255D'))))
+        for i in range(xhalf-fifth, xhalf+fifth):
+            term.puts(i, yhalf-2, "{}".format(chr(toInt('2550'))))
+            term.puts(i, yhalf, "{}".format(chr(toInt('2550'))))
+        for j in range(yhalf-2, yhalf+1):
+            term.puts(xhalf-fifth, j, "{}".format(chr(toInt('2551'))))
+            term.puts(xhalf+fifth, j, "{}".format(chr(toInt('2551'))))
+
+        term.puts(xhalf-fifth, yhalf-2, "{}".format(chr(toInt('2554'))))
+        term.puts(xhalf+fifth, yhalf-2, "{}".format(chr(toInt('2557'))))
+        term.puts(xhalf-fifth, yhalf, "{}".format(chr(toInt('255A'))))
+        term.puts(xhalf+fifth, yhalf, "{}".format(chr(toInt('255D'))))
+
+
     direction_name = 'Enter in your name or leave blank for a random name'
     direction_exit = 'Press [[ESC]] if you wish to exit character creation'
-
-    x = SCREEN_WIDTH//2
-    w = SCREEN_WIDTH//5
-    y = SCREEN_HEIGHT//2
-    #term.clear_(x-w, y-3, x, 6)area
+    xhalf = SCREEN_WIDTH//2
+    yhalf = SCREEN_HEIGHT//2
+    fifth = SCREEN_WIDTH//5
 
     term.clear()
     border()
-    term.puts(center(direction_name, x*2), y-4, direction_name)
-    term.puts(center(direction_exit[2:], x*2), y+2, direction_exit)
-    result, text = term.read_str(x-w+1, y-1, "", 30)
+    text()
+    result, text = term.read_str(xhalf-fifth+1, yhalf-1, "", 30)
 
     if result:
         term.bkcolor("white")
-        term.puts(x-w+1, y-1, "[color=black]{}[/color]".format(text))
+        term.puts(xhalf-fifth+1, yhalf-1, "[color=black]{}[/color]".format(text))
         term.bkcolor("black")
+        term.refresh()
+        
     return output(proceed=result, value=text)
 
 
