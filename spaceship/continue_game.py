0f46c35a6db79c5bfd2c0fc9ff0a6c014eb1be83
Sam Whang <sangwoowhang@gmail.com>
2017-11-17 11:58:10 -0600

    Still adding changes to save game screen ui

--- spaceship/continue_game.py
+++ spaceship/continue_game.py
@@ -1,39 +1,54 @@
 import sys
 import os
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__))+'/../')
-from spaceship.constants import MENU_SCREEN_WIDTH as SCREEN_WIDTH
-from spaceship.constants import MENU_SCREEN_HEIGHT as SCREEN_HEIGHT
 from spaceship.screen_functions import *
 from bearlibterminal import terminal as term
 from spaceship.setup_game import setup
 import shelve
 # Begin continue Menu
 def continue_game():
+
+    def get_saves():
+        '''Retrieves save files'''
+        save_files = []
+        for root, dirs, files in os.walk('saves', topdown=True):
+            # get all files in the save directory and parse their file names
+            for f in files:
+                if f.endswith('.dat'):
+                    save_files.append(f.replace('.dat', ''))
+        return save_files
+
     def no_saves():
+        '''Prints no save ui screen to terminal'''
         term.puts(
             center(
                 'No Saved Games', 
                 term.state(term.TK_WIDTH)), 
             term.state(term.TK_HEIGHT)//2, 
             'NO SAVED GAMES')
-    
-    def saves_exists():
-        for root, dirs, files in os.walk('saves', topdown=True):
-            for f in files:
-                if f.endswith('.dat'):
-                    save_files.append(f.replace('.dat', ''))
 
+    def saves_exists():
+        '''Prints saved files to terminal'''
+        # save screen header
+        for i in range(term.state(term.TK_WIDTH)):
+            term.puts(i, 0, '#')
+        term.puts(center('Saved Files  ', term.state(term.TK_WIDTH)), 0, ' Saved Files ')
+
+        # list the saved files
         for i, save in enumerate(save_files):
+            if i == save_index:
+                save = "[c=#00FFFF]{}[/c]".format(save)
             term.puts(1, 3 + i, save)
     
     save_index = 0
-    save_files = []
+    save_files = get_saves()
     while True:    
         term.clear()
         if not os.path.isdir('saves') or os.listdir('saves') == []:
             no_saves()
         else:
             saves_exists()
+
         term.refresh()
         code = term.read()
 
@@ -45,8 +60,13 @@ def continue_game():
 
             print(player.name, world_map)
             exit("Finished opening file")
+
+        elif 
+
         elif code == term.TK_ESCAPE:
             break
+
+
 
     return True
 # End Continue Menu
