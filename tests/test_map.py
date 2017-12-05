019cf80d5b1ca7f0bcea2066a8f24990ae34da59
Sam Whang <sangwoowhang@gmail.com>
2017-12-05 11:55:21 -0600

    Working on ai -- taking a different approach

--- tests/test_map.py
+++ tests/test_map.py
@@ -4,29 +4,35 @@ sys.path.insert(0, os.path.dirname(os.path.abspath(__file__))+'/../')
 from bearlibterminal import terminal as t
 from spaceship.maps.city import City
 from spaceship.maps.cave import Cave
-from spaceship.units.monsters import Rat
+from spaceship.units.monsters import Rat, Bat
 from spaceship.action import commands
 '''Test map with key handling functionality to test player input in controlled layouts'''
 
 test_map_path = "./assets/maps/test_map.png"
 
 def main():
+    bat = Bat(15, 10)
     rat = Rat(33, 11)
     cave = Cave(66, 22)
+    cave.add_units([bat, rat])
     t.open()
-    while True:
-        t.clear()
-        cave.fov_calc([(rat.x, rat.y, rat.sight)])
-        for x, y, col, char in cave.output(rat.x, rat.y):
-            t.puts(x, y, "[c={}]{}[/c]".format(col, char))
-        t.refresh()
-        k = t.read()
-        if k in (t.TK_UP, t.TK_DOWN, t.TK_LEFT, t.TK_RIGHT):
-            if not t.state(t.TK_SHIFT):
-                x, y, a, act = commands[(k, 0)]
-                rat.move(x, y)
-        else:
-            break
-        
+    proceed=True
+    while proceed:
+        for unit in [bat, rat]:
+            t.clear()
+            cave.fov_calc([(unit.x, unit.y, unit.sight)])
+            for x, y, col, char in cave.output(unit.x, unit.y):
+                t.puts(x, y, "[c={}]{}[/c]".format(col, char))
+            t.refresh()
+
+            k = t.read()
+            if k in (t.TK_UP, t.TK_DOWN, t.TK_LEFT, t.TK_RIGHT, t.TK_KP_5):
+                if not t.state(t.TK_SHIFT):
+                    x, y, a, act = commands[(k, 0)]
+                    unit.move(x, y)
+            else:
+                proceed=False
+                break
+
 if __name__ == "__main__":
     main()
