c49e6b64db23521414c0be5045543441465095c8
Sam Whang <sangwoowhang@gmail.com>
2017-12-06 15:51:52 -0600

    fixed import conflict when running python vs pytest

--- tests/test_objects_base_object.py
+++ tests/test_objects_base_object.py
@@ -1,11 +1,14 @@
+from .utils import spaceship
 from spaceship.objects.base import Object
 import math
 
 def test_object_position():
     obj = Object(5, 5, "a", "#000000", "#ffffff")
+    
     assert obj.position == (5, 5)
 
 def test_object_distance():
     a = Object(0, 0, "a", "#000000", "#ffffff")
     b = Object(9, 9, "b", "#000000", "#ffffff")
-    print(a.distance(b))
+    
+    assert a.distance(b) > 0
