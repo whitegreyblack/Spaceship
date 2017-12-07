fd99d4a35118ea2d3b5a56736400786e45de859c
Sam Whang <sangwoowhang@gmail.com>
2017-12-07 10:29:53 -0600

    Moved maps to classes folder and begun working with new tile objects

---
+++ spaceship/classes/tile.py
@@ -0,0 +1,20 @@
+class Tile:
+    def __init__(self, ch, fg, bg):
+        self.character = ch
+        self.foreground = fg
+        self.background = bg
+
+    def draw(self):
+        return self.character, self.foreground, self.background
+
+class WorldTile(Tile):
+    def __init__(self, ch, fg, bg, land, enterable):
+        super().__init__(ch, fg, bg)
+        self.land = land
+        self.enterable = enterable
+
+class MapTile(Tile):
+    def __init__(self, ch, fg, block, light):
+        super().__init__(ch, fg, bg)
+        self.block = block
+        self.light = light
