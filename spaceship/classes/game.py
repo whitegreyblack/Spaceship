164301371dd92d30ecd86d0505ffe5d0a9265ac7
Sam Whang <sangwoowhang@gmail.com>
2017-12-14 08:44:48 -0600

    Added more content in game.py

--- spaceship/classes/game.py
+++ spaceship/classes/game.py
@@ -4,15 +4,45 @@ class Point:
     def __init__(self):
         self.x, self.y = x, y
 
+    def __iadd__(self, other):
+        return Point(self.x + other.x, self.y + other.y)
+
+    def __isub__(self, other):
+        return Point(self.x - other.x, self.y - other.y)
+
 class Tile:
-    pass
+    def __init__(self, ch, fg, bg):
+        self.character = ch
+        self.foreground = fg
+        self.background = bg
 
 class Object:
-    pass
+    def __init__(self, point, tile):
+        self.point = point
+        self.tile = tile
 
-class Unit:
+    def position(self):
+        return x, y
+
+    def character(self):
+        return self.tile.character
+
+    def foreground(self):
+        return self.tile.foreground
+
+    def background(self):
+        return self.tile.background
+
+class Unit(Object):
+    def __init__(self, point, tile):
+        super().__init__()
+    
+    def move(self, point):
+        self.point += point
+
+class Monster(Unit):
     def __init__(self):
-        pass
+        super().__init__()
 
 class Game:
     def __init__(self, font="default"):
