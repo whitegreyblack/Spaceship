2d4c2a608d30ad9b25f62dc6a407fac3a3555792
Sam Whang <sangwoowhang@gmail.com>
2018-01-23 17:16:47 -0600

    removed example codes

--- other/examples/fontviewer.py
+++
@@ -1,8 +0,0 @@
-import sys
-from bearlibterminal import terminal
-
-if __name__ == '__main__':
-    terminal.open()
-    terminal.set("U+E000: ./fonts/scientifica.bdf, size=8x8, align=center")
-    terminal.refresh()
-    terminal.read()
