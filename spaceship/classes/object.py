b52e07eeaf3445e165d5a8d5b9ceae7b52421d12
Sam Whang <sangwoowhang@gmail.com>
2018-01-24 09:58:10 -0600

    Refactored out tile class from object

--- spaceship/classes/object.py
+++ spaceship/classes/object.py
@@ -20,10 +20,14 @@ class Point:
         return self.x, self.y
 
     @position.setter
-    def position(self, xy):
-        dx, dy = xy
-        self.x += dx
-        self.y += dy
+    def position(self, point):
+        self = point
+
+    def move(self, other):
+        try:
+            self.x, self.y = self.x + other.x, self.y + other.y
+        except AttributeError:
+            self.x, self.y = self.x + other[0], self.y + other[1]
 
     def __repr__(self):
         return f"{self.__class__.__name__}: ({self.x}, {self.y})"
@@ -33,6 +37,14 @@ class Point:
             x, y = self.x + other.x, self.y + other.y
         except AttributeError:
             x, y = self.x + other[0], self.y + other[1]
+        finally:
+            return Point(x, y)
+
+    def __sub__(self, other):
+        try:
+            x, y = self.x - other.x, self.y - other.y
+        except:
+            x, y = self.x - other[0], self.y - other[1]
         finally:
             return Point(x, y)
 
@@ -64,62 +76,41 @@ class Point:
         return math.sqrt(
             math.pow(other.x - self.x, 2) + math.pow(other.y - self.y, 2))
 
-class Tile:
-    __slots__ = ('character', 'foreground', 'background')
-    def __init__(self, character, foreground="white", background="black"):
-        self.character = character
-        self.foreground = foreground 
-        self.background = background
-    
-    @property
-    def color(self):
-        return self.foreground, self.background
-
 class Object:
     '''Base object class used in the following subclasses:
-        
-    Tiles :- WorldTiles, MapTiles
-
     Units :- NPC's, Monsters, Player
-
-    Items :- Armor, Weapons, Usables
-
-    Implements position and object position interactions
-
-    Position :- Changes constantly so getter/setter
-
-    Graphics :- Doesn't usually change -- for now set as only getter
     '''
     object_id = 0
     
     def __init__(self, x: int, y: int, ch: chr, fg: str, bg: str) -> None:
         Object.object_id += 1
-        self.tile = Tile(ch, fg, bg)
+        self.character = ch
+        self.foreground = fg
+        self.background = bg
         self.local = Point(x, y)
-        print(self.local.position)
 
     def __str__(self) -> str:
         return "{}: (x={}, y={}, ch={}, fg={}, bg={})".format(
             self.__class__.__name__, 
             *self.local.position,
-            self.tile.character,
-            *self.tile.color)
+            self.character,
+            *self.color)
+
+    @property
+    def color(self):
+        return self.foreground, self.background
 
     def output(self):
-        return (*self.local.position, self.tile.char, *self.color())
-
-    def distance(self, other):
-        return self.local.distance(other.local)
+        return (*self.local.position, self.character, *self.color)
 
 if __name__ == "__main__":
     # unit tests?
     obj = Object(5, 5, 'o', "#ffffff", "#000000")
     print(obj)
     print(obj.local.position)
+    print(obj.output())
 
     a = Object(0, 0, "a", "#000000", "#ffffff")
     b = Object(9, 9, "b", "#000000", "#ffffff")
-    
-    print(a.distance(b))
     print(a.local.distance(b.local))
     print(Object.object_id)
---
+++ tests/test_point.py
@@ -0,0 +1,49 @@
+from spaceship.classes.object import Point
+
+def test_point_init():
+    p = Point(0, 0)
+    assert p.position == (0, 0)
+
+    p = Point(1, 2)
+    assert p.position == (1, 2)
+
+def test_point_move_point():
+    a = Point(1, 2)
+    a.move(Point(2, 1))
+    assert a.position == (3, 3)
+
+def test_point_move_tuple():
+    a = Point(1, 2)
+    a.move((2, 1))
+    assert a.position == (3, 3)
+
+def test_point_add_point():
+    c = Point(1, 2) + Point(2, 1)
+    assert c.position == (3, 3)
+
+def test_point_add_tuple():
+    c = Point(1, 2) + (2, 1)
+    assert c.position == (3, 3)
+
+def test_point_position_add_point():
+    c = Point(2, 1) + Point(1, 2).position
+    assert c.position == (3, 3)
+
+def test_point_position_sub_point():
+    c = Point(2, 1) - Point(1, 2).position
+    assert c.position == (1, -1)
+
+def test_point_iadd_point():
+    a = Point(1, 2)
+    a += Point(2, 1)
+    assert a.position == (3, 3)
+
+def test_point_iadd_tuple():
+    a = Point(1, 2)
+    a += (2, 1)
+    assert a.position == (3, 3)
+
+def test_point_isub_point():
+    a = Point(1, 2)
+    a -= Point(2, 1)
+    assert a.position == (-1, 1)
