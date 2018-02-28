05bd6ba67dd9d21918f3b336d32dbfd6225b63d6
Sam Whang <sangwoowhang@gmail.com>
2018-02-28 10:29:26 -0600

    Modified stats to parse all different args in __init__ instead of in a class method

--- spaceship/classes/stats.py
+++ spaceship/classes/stats.py
@@ -3,19 +3,33 @@ from die import Die
 import random
 import re
 
+class Example:
+    def __init__(self, stats):
+        self.stats = Stats(stats)
+
 class Stats:
     '''Stats componenet used in unit classes'''
-    def __init__(self, values):
-        self.str, self.con, self.agi, self.int, self.wis, self.luc = values
+    __slots__ = ['str', 'con', 'agi', 'int', 'wis', 'luc']
+    def __init__(self, stats):
+        string_single = isinstance(stats, str)
+        if string_single:
+            stats = stats.split()
 
-    @classmethod
-    def construct(cls, strings):
-        '''Creates a valid stats object using a die string as
-        stat parameters instead of normal integer values
-        '''
-        if isinstance(strings, str):
-            strings = strings.split() 
-        return cls(next(Die.construct(s).roll()) for s in strings)
+        string_multiple = all([isinstance(s, str) for s in stats])
+        if string_single or string_multiple:
+            stats = [next(Die.construct(stat).roll()) for stat in stats]
+
+        for attr, stat in zip(self.__slots__, stats):
+            setattr(self, attr, stat)
+
+    # @classmethod
+    # def construct(cls, strings):
+    #     '''Creates a valid stats object using a die string as
+    #     stat parameters instead of normal integer values
+    #     '''
+    #     if isinstance(strings, str):
+    #         strings = strings.split() 
+    #     return cls(next(Die.construct(s).roll()) for s in strings)
 
     def __repr__(self):
         '''Returns stat information for dev'''
@@ -23,19 +37,20 @@ class Stats:
 
     def __str__(self):
         '''Returns stat information for user'''
-        return \
-            f"STR: {self.str}, CON: {self.con}, AGI: {self.agi}, INT: {self.int}, WIS: {self.wis}, LUC: {self.luc}"
+        return ", ".join(f'{s.upper()}: {getattr(self, s)}' for s in self.__slots__)
 
 if __name__ == "__main__":
-    s = Stats((1, 1, 1, 1, 1, 1))
+    s = Stats((1, 2, 3, 4, 5, 6))
     print(s)
 
-    s = Stats.construct(("1d6", "1d6", "1d6", "1d6", "1d6", "1d6"))
+    s = Stats("1d6 1d6 1d6 1d6 1d6 1d6")
     print(s)
 
-    s = Stats.construct(("1d6 1d6 1d6 1d6 1d6 1d6"))
+    s = Stats("1d6 1d6 1d6 1d6 1d6 1d6".split())
     print(s)
 
-    s = Stats.construct(("1d6+3", "1d6", "1d6", "1d6", "1d6", "1d6"))
-    print(s)
-    print(repr(s))
+    e = Example("1d6 1d6 1d6 1d6 1d6 1d6")
+    classes = [e]
+    for c in classes:
+        if hasattr(c, 'stats') and isinstance(c.stats, Stats):
+            print(e.stats)
