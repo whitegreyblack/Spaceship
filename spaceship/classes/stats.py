5d2ef1389ac8fb68881f65122c50cec8eb183802
Sam Whang <sangwoowhang@gmail.com>
2018-02-28 09:58:21 -0600

    Added a condition in stat constructor to split string if a string of die rolls are given

--- spaceship/classes/stats.py
+++ spaceship/classes/stats.py
@@ -5,7 +5,6 @@ import re
 
 class Stats:
     '''Stats componenet used in unit classes'''
-
     def __init__(self, values):
         self.str, self.con, self.agi, self.int, self.wis, self.luc = values
 
@@ -14,10 +13,13 @@ class Stats:
         '''Creates a valid stats object using a die string as
         stat parameters instead of normal integer values
         '''
+        if isinstance(strings, str):
+            strings = strings.split() 
         return cls(next(Die.construct(s).roll()) for s in strings)
 
     def __repr__(self):
-        return str(self)
+        '''Returns stat information for dev'''
+        return f'{self.__class__.__name__.upper()}: ({self})'
 
     def __str__(self):
         '''Returns stat information for user'''
@@ -31,5 +33,9 @@ if __name__ == "__main__":
     s = Stats.construct(("1d6", "1d6", "1d6", "1d6", "1d6", "1d6"))
     print(s)
 
+    s = Stats.construct(("1d6 1d6 1d6 1d6 1d6 1d6"))
+    print(s)
+
     s = Stats.construct(("1d6+3", "1d6", "1d6", "1d6", "1d6", "1d6"))
     print(s)
+    print(repr(s))
