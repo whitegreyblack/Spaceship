ec48f9e402aba3123f86e778e139d1a09750560f
Sam Whang <sangwoowhang@gmail.com>
2018-03-05 16:53:56 -0600

    Added enemy factory?

--- ecs/entities.py
+++ ecs/entities.py
@@ -1,5 +1,6 @@
 from ecs import Entity
 import components
+from die import Die
 
 def make_sword():
     return Entity(description=components.Description(
@@ -18,9 +19,26 @@ def make_armor():
                     'armor': components.Defense(3),
     })
 
+def make_player(name="Hero"):
+    return Entity(description=components.Description(name=name),
+        components={
+            'render': components.Render('@'),
+            'damage': components.Damage('1d4'),
+            'armor': components.Defense(2),
+        })
+
+def make_enemy(name="rat", symbol="r"):
+    return Entity(description=components.Description(name=name),
+        components={
+            'render': components.Render(symbol),
+            'damage': components.Damage('2d3'),
+            'armor': components.Defense(1),
+        })
+
 if __name__ == "__main__":
+    p = make_player()
+    print(p)
     s = make_sword()
     print(s)
     a = make_armor()
     print(a)
-    print(repr(a))
