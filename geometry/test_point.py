8f716bafb9044fcadaeb46ed2da3d1ea01eee110
Sam Whang <swhang@eaglecrk.com>
2018-08-03 11:55:26 -0500

    Fixed point to pass tests

--- geometry/test_point.py
+++ geometry/test_point.py
@@ -2,15 +2,15 @@
 from point import Point
 
 def test_init():
-	p = Point(3, 4)
-	assert p.x == 3
-	assert p.y == 4
-	
+    p = Point(3, 4)
+    assert p.x == 3
+    assert p.y == 4
+
 def test_init_tuple():
-	p = Point.from_tuple((3, 2))
-	assert p.x == 3
-	assert p.y == 2
+    p = Point.from_tuple((3, 2))
+    assert p.x == 3
+    assert p.y == 2
 
 def test_eq():
-	p = Point(4, 3)
-	assert p == (4, 3)
+    p = Point(4, 3)
+    assert p == (4, 3)
